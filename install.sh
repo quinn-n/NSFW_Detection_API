a755701f551f8cab74f4fc9fa2adb54d446a80ce
quinn-n <quinnneufeld@protonmail.com>
2023-04-22 16:16:23 -0400

    added install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install python dependencies inside pew env
+pew new -a `pwd` nsfwdetector
+pip install -r requirements.txt
+
+# Create user systemd service
+mkdir -p ~/.config/systemd/user
+cp nsfwdetector.service ~/.config/systemd/user
+systemctl --user daemon-reload
+
+echo "Run `systemctl enable --user --now nsfwdetector` to enable & start the service."
