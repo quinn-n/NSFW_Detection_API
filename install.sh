c6928980af214939150cda86b880b0e54ad71054
quinn-n <quinnneufeld@protonmail.com>
2023-04-23 23:45:39 -0400

    fixed systemd unit install path hopefully

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ pew new -a `pwd` nsfwdetector -d
 pew in nsfwdetector pip install -r requirements.txt
 
 # Create user systemd service
-cp nsfwdetector.service /usr/local/lib/systemd/system/
+cp nsfwdetector.service /etc/systemd/system/
 systemctl --user daemon-reload
 
 echo "Run 'systemctl enable --user --now nsfwdetector' to enable & start the service."
